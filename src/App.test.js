c58eb0db5f0ccdce1c182d5aa5575e99a677d4ba
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-12-18 17:37:17 +0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -12,9 +12,3 @@ test('url is correct', () => {
   const linkElement = screen.getByTestId('learn-link');
   expect(linkElement.href).toContain('ultimateqa.com');
 });
-
-test('url is correct', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId('learn-link');
-  expect(linkElement.href).toContain('ultimateqa.com');
-});
